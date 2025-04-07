104e0a58f6db0d3e3ee2b86b874bac3afb531dc2
Mayra Silva <mayraeduarda2002@gmail.com>
2025-04-07 14:45:29 -0300

    define variables

--- regular_expressions_by_building_a_password_generator.py
+++ regular_expressions_by_building_a_password_generator.py
@@ -1,1 +1,10 @@
 #Beginning
+import string
+
+
+
+letters = string.ascii_letters #these are the letters abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+digits = string.digits #these are the digits 0123456789
+symbols = string.punctuation # these are the symbols !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+
+print(letters)
