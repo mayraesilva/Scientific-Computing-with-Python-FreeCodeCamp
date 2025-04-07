2ea5edb370b4fd5c6bc27ddbc4d018ef33950f50
Mayra Silva <mayraeduarda2002@gmail.com>
2025-04-07 15:57:05 -0300

    pausa do dia

--- regular_expressions_by_building_a_password_generator.py
+++ regular_expressions_by_building_a_password_generator.py
@@ -5,7 +5,7 @@ import string
 
 
 
-def generate_password():
+def generate_password(length):
     #Define the possible characters for the password
     letters = string.ascii_letters #these are the letters abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
     digits = string.digits #these are the digits 0123456789
@@ -15,10 +15,16 @@ def generate_password():
     #Combine all characters
     all_characters = letters + digits + symbols
 
-
     # print(all_characters)
     # print(secrets.choice(all_characters))
+
+    password = ''
+
+    #Generate password
+    for i in range(length):
+        password += secrets.choice(all_characters) 
     
+
 
 
 
