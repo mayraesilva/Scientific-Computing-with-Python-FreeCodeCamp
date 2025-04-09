37ca71c57d3e5a0eb4b587fb96e3237e5f42f823
Mayra Silva <mayraeduarda2002@gmail.com>
2025-04-09 10:11:07 -0300

    using __name__

--- password_generator.py
+++ password_generator.py
@@ -45,8 +45,9 @@ def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase
     return password
 
 
-new_password = generate_password()
-print('Generated password:', new_password)
+if __name__ == '__main__':
+    new_password = generate_password()
+    print('Generated password:', new_password)
 
 # pattern = re.compile('l+') 
 # quote = 'Not all those who wander are lost.'
