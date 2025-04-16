9ea43cc81d76b2b163ec97b6e9b2b54a643486c1
Mayra Silva <mayraeduarda2002@gmail.com>
2025-04-16 14:08:45 -0300

    step 35

--- algorithm_design_shortest_path.py
+++ algorithm_design_shortest_path.py
@@ -23,8 +23,8 @@ my_graph = {
 
 def shortest_path(graph, start):
     unvisited = list(graph)
-    distances = {}
-    paths = {key : [] for key in graph}
+    distances = {node : 0 if node == start else float('inf') for node in graph}
+    paths = {node : [] for node in graph}
 
     
     print(f'Unvisited: {unvisited}\nDistances: {distances}')
