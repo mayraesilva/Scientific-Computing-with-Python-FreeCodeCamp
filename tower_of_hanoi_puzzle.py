8813c9200753065be6b5c4245668ab81435f349b
Mayra Silva <mayraeduarda2002@gmail.com>
2025-04-24 15:32:51 -0300

    begin function

--- tower_of_hanoi_puzzle.py
+++ tower_of_hanoi_puzzle.py
@@ -4,4 +4,16 @@
 # a number of disks of different diameters.
 
 
-rods = {}
+rods = {
+    'A': list(range(3, 0, -1),
+    'B': [],
+    'C': []
+    }
+
+# You can move only top-most disks
+# You can move only one disk at a time
+# You cannot place larger disks on top of smaller ones
+
+
+def move():
+    pass
