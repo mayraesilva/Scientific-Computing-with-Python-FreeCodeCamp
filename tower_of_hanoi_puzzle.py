9cb4229a56f42229b1832336c37703a5baca4414
Mayra Silva <mayraeduarda2002@gmail.com>
2025-04-24 15:11:12 -0300

    tower of hanoi step 1

---
+++ tower_of_hanoi_puzzle.py
@@ -0,0 +1,7 @@
+#In this project, you will solve the mathematical puzzle 
+# known as the Tower of Hanoi. 
+# The puzzle consists of three rods and 
+# a number of disks of different diameters.
+
+
+rods = {}
