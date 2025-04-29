450d1445324e31bc14b10ff2d3e275a1559ef97c
Mayra Silva <mayraeduarda2002@gmail.com>
2025-04-29 17:49:39 -0300

    step 27

--- merge_sort_algorithm.py
+++ merge_sort_algorithm.py
@@ -1,6 +1,11 @@
 #Mayra Silva step by step with comments 
 
 def merge_sort(array):
+
+    if len(array) <= 1:
+        return
+
+
     middle_point = len(array) // 2 # // integer division
     left_part = array[:middle_point]
     right_part = array[middle_point:]
@@ -12,3 +17,26 @@ def merge_sort(array):
     left_array_index = 0
     right_array_index = 0
     sorted_index = 0
+
+
+    while left_array_index < len(left_part) and right_array_index < len(right_part):
+
+        if left_part[left_array_index] < right_part[right_array_index]:
+            array[sorted_index] = left_part[left_array_index]
+            left_array_index += 1
+        
+        else:
+            array[sorted_index] = right_part[right_array_index]
+            right_array_index  += 1
+
+        sorted_index += 1
+
+    while left_array_index < len(left_part):
+        array[sorted_index] = left_part[left_array_index] 
+        left_array_index += 1
+        sorted_index += 1
+
+    while right_array_index < len(right_part):
+        array[sorted_index] = right_part[right_array_index]
+        right_array_index += 1
+        sorted_index += 1
