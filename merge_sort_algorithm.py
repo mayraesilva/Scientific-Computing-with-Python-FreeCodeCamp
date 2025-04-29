c70e8681ac55fa585f97f9928239c813b243e5d4
Mayra Silva <mayraeduarda2002@gmail.com>
2025-04-29 17:03:32 -0300

    merge sort step 11

---
+++ merge_sort_algorithm.py
@@ -0,0 +1,14 @@
+#Mayra Silva step by step with comments 
+
+def merge_sort(array):
+    middle_point = len(array) // 2 # // integer division
+    left_part = array[:middle_point]
+    right_part = array[middle_point:]
+
+    merge_sort(left_part)
+    merge_sort(right_part)
+
+
+    left_array_index = 0
+    right_array_index = 0
+    sorted_index = 0
