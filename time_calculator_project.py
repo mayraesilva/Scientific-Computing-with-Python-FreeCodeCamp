b8f2345bb820ee8e6d08ab9026ba58fd126e30f1
Mayra Silva <mayraeduarda2002@gmail.com>
2025-04-30 13:30:46 -0300

    step 1

--- time_calculator_project.py
+++ time_calculator_project.py
@@ -1,1 +1,13 @@
 #Building a Time calculator by Mayra Silva
+
+def add_time(start, duration, day_of_week=None):
+    separator = ':'
+    time_of_the_day = ['AM', 'PM']
+
+    start_time = start.split()
+    print(start_time)
+
+    pass
+
+
+add_time('3:00 PM', '3:10', 'Sunday')
