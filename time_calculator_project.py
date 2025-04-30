91401e95fbec3b02edd2167b31e045430a1d5e0f
Mayra Silva <mayraeduarda2002@gmail.com>
2025-04-30 14:21:18 -0300

    format time step 1

--- time_calculator_project.py
+++ time_calculator_project.py
@@ -1,11 +1,21 @@
 #Building a Time calculator by Mayra Silva
 
+def format_time(time):
+    time = '3:00 PM'
+    part_of_the_day = ['AM', 'PM']
+
+    splitted_time = time.split()
+    print(splitted_time)
+
+format_time('3:00 PM')
+
+
 def add_time(start, duration, day_of_week=None):
     separator = ':'
     time_of_the_day = ['AM', 'PM']
 
     start_time = start.split()
-    print(start_time)
+    #print(start_time)
 
     pass
 
