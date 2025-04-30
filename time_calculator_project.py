848b9de0b08b2fcb13d27e0e5eae716c54ebd174
Mayra Silva <mayraeduarda2002@gmail.com>
2025-04-30 15:31:22 -0300

    calculate step 1

--- time_calculator_project.py
+++ time_calculator_project.py
@@ -1,16 +1,20 @@
 #Building a Time calculator by Mayra Silva
 
 def format_time(time):
-
+    
     separator_hours_minutes = ':'
     
 
     splitted_time = time.split() #use space as separator
     print(splitted_time)
-    time_of_the_day = splitted_time.pop() #remove the string of part of the day (AM, PM)
-    print(time_of_the_day)
-    print(splitted_time)
 
+
+
+    if len(splitted_time) >= 2:
+        time_of_the_day = splitted_time.pop() #remove the string of part of the day (AM, PM)
+        print(time_of_the_day)
+        print(splitted_time)
+        
 
     splitted_time_hours_and_minutes = splitted_time[0].split(separator_hours_minutes) #separate hours and minutes
     print(splitted_time_hours_and_minutes)
@@ -20,7 +24,10 @@ def format_time(time):
     if int(minutes) >= 60:
         raise ValueError('Minutes cannot be equal or greater than 60')
     
-    splitted_time_hours_and_minutes = splitted_time_hours_and_minutes + [time_of_the_day]
+
+    if len(splitted_time) >= 2:
+        splitted_time_hours_and_minutes = splitted_time_hours_and_minutes + [time_of_the_day]
+    
     print(splitted_time_hours_and_minutes)
 
     return splitted_time_hours_and_minutes
@@ -28,18 +35,25 @@ def format_time(time):
 
 format_time('3:00 PM')
 
+def calculate_time_passed(moment_of_start, time_of_duration):
+    time_of_day = ['AM', 'PM']
+
+    start_hours = moment_of_start[0]
+    start_minutes = moment_of_start[1]
+
+    
+
+
+
 
 
 
 
 def add_time(start, duration, day_of_week=None):
-    separator = ':'
-    time_of_the_day = ['AM', 'PM']
-
-    start_time = start.split()
-    #print(start_time)
+    moment_of_start = format_time(start)
+    time_of_duration = format_time(duration)
 
     pass
 
 
-add_time('3:00 PM', '3:10', 'Sunday')
+#add_time('3:00 PM', '3:10', 'Sunday')
