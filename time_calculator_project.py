05b8ba549cbd8ce1b36e1a685e8dfe2e06187f6b
Mayra Silva <mayraeduarda2002@gmail.com>
2025-04-30 15:02:54 -0300

    format time concluded

--- time_calculator_project.py
+++ time_calculator_project.py
@@ -1,26 +1,35 @@
 #Building a Time calculator by Mayra Silva
 
 def format_time(time):
-    time = '3:00 PM'
+
     separator_hours_minutes = ':'
-    part_of_the_day = ['AM', 'PM']
     
 
-    splitted_time = time.split()
+    splitted_time = time.split() #use space as separator
     print(splitted_time)
-    time_of_the_day = splitted_time.pop()
+    time_of_the_day = splitted_time.pop() #remove the string of part of the day (AM, PM)
     print(time_of_the_day)
     print(splitted_time)
 
 
-    splitted_time_hours_and_minutes = splitted_time[0].split(separator_hours_minutes)
+    splitted_time_hours_and_minutes = splitted_time[0].split(separator_hours_minutes) #separate hours and minutes
     print(splitted_time_hours_and_minutes)
     hours = splitted_time_hours_and_minutes[0]
     minutes = splitted_time_hours_and_minutes[1]
 
+    if int(minutes) >= 60:
+        raise ValueError('Minutes cannot be equal or greater than 60')
+    
+    splitted_time_hours_and_minutes = splitted_time_hours_and_minutes + [time_of_the_day]
+    print(splitted_time_hours_and_minutes)
+
+    return splitted_time_hours_and_minutes
     
 
 format_time('3:00 PM')
+
+
+
 
 
 def add_time(start, duration, day_of_week=None):
