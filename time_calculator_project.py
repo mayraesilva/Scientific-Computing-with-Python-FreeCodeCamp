3b901d8eaec856fec7376eb4c2aa8d171872e943
Mayra Silva <mayraeduarda2002@gmail.com>
2025-04-30 14:42:41 -0300

    split time

--- time_calculator_project.py
+++ time_calculator_project.py
@@ -2,10 +2,23 @@
 
 def format_time(time):
     time = '3:00 PM'
+    separator_hours_minutes = ':'
     part_of_the_day = ['AM', 'PM']
+    
 
     splitted_time = time.split()
     print(splitted_time)
+    time_of_the_day = splitted_time.pop()
+    print(time_of_the_day)
+    print(splitted_time)
+
+
+    splitted_time_hours_and_minutes = splitted_time[0].split(separator_hours_minutes)
+    print(splitted_time_hours_and_minutes)
+    hours = splitted_time_hours_and_minutes[0]
+    minutes = splitted_time_hours_and_minutes[1]
+
+    
 
 format_time('3:00 PM')
 
