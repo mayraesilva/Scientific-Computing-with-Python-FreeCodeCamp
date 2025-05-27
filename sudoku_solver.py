6bc32b5543ae66a3bd893b14d214ff2d36edd3d1
Mayra Silva <mayraeduarda2002@gmail.com>
2025-05-27 16:24:12 -0300

    sudoku step 14

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -4,6 +4,9 @@ class Board:
     def __init__(self, board):
         self.board = board
 
+    def find_empty_cell(self):
+        pass
+
 
 puzzle = [
   [0, 0, 2, 0, 0, 8, 0, 0, 0],
@@ -18,5 +21,5 @@ puzzle = [
 ]
 
 gameboard = Board(puzzle) #instance of a class
-print(gameboard.board)
+
 
