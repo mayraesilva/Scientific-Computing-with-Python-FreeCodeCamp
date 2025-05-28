7e77366e213c9735213647d3e76c9a6ec2b0f33f
Mayra Silva <mayraeduarda2002@gmail.com>
2025-05-28 12:26:23 -0300

    step 32 sudoku

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -18,7 +18,8 @@ class Board:
         return num not in self.board[row]
     
     def valid_in_col(self, col, num):
-        pass
+        return all(self.board[row][col] != num for row in range(9))
+
 
 
 puzzle = [
