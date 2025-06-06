6e06c0582ac111a3effe4f99dd26878bf90e287c
Mayra Silva <mayraeduarda2002@gmail.com>
2025-06-06 14:51:24 -0300

    step 25 - bst

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -38,5 +38,5 @@ class BinarySearchTree:
         return self._search(node.right, key)
     
     def search(self, key):
-        self._search(self.root, key)
+        return self._search(self.root, key)
 
