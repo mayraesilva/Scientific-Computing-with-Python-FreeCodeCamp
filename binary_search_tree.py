a7f0ee9a88b4f3c4db5e9b547a809f7f578ebad0
Mayra Silva <mayraeduarda2002@gmail.com>
2025-06-06 15:06:57 -0300

    step 33 - bst

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -43,6 +43,9 @@ class BinarySearchTree:
     def search(self, key):
         return self._search(self.root, key)
     
+    def _delete(self, node, key):
+        pass
+    
 
 bst = BinarySearchTree()
 nodes = [50, 30, 20, 40, 70, 60, 80]
@@ -50,4 +53,4 @@ nodes = [50, 30, 20, 40, 70, 60, 80]
 for node in nodes:
     bst.insert(node)
 
-print('Search for 80:', bst.search(80))
+#print('Search for 80:', bst.search(80))
