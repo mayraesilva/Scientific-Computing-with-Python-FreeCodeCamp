9dd8b3327c5f720174412c1220e31fe4685f530b
Mayra Silva <mayraeduarda2002@gmail.com>
2025-06-23 17:34:12 -0300

    step 42 - bst

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -62,7 +62,8 @@ class BinarySearchTree:
                 return node.left
             
     def _min_value(self, node):
-        pass
+        while node.left is not None:
+            pass
     
 
 bst = BinarySearchTree()
