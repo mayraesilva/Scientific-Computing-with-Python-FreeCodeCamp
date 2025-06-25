f4f6f7e9906e310061c51def7e974f457ff6167a
Mayra Silva <mayraeduarda2002@gmail.com>
2025-06-25 16:45:21 -0300

    step 60 - bst

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -109,6 +109,10 @@ nodes = [50, 30, 20, 40, 70, 60, 80]
 for node in nodes:
     bst.insert(node)
 
+
 print('Search for 80:', bst.search(80))
+
 print('Inorder traversal:', bst.inorder_traversal())
+
+bst.delete(40)
 
