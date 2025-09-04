cb200d3e587331eefcce76c6ba304cb865c6e205
Mayra Silva <mayraeduarda2002@gmail.com>
2025-09-04 17:06:01 -0300

    vector space steps 12-15

--- building_a_vector_space.py
+++ building_a_vector_space.py
@@ -16,6 +16,11 @@ class R2Vector(): #Two dimensions vectors
         return f'{vector}'
     
 
+class R3Vector(R2Vector): #Child of R2Vector
+    def __init__(self, x, y, z):
+        super().__init__(x,y) # The super() function enables us to refer implicitly to the parent class, it is calling __ini__ from the parent class
+        self.z = z
+
 
 v1 = R2Vector(2,3)
 print(v1.norm()) # to check if it is working
