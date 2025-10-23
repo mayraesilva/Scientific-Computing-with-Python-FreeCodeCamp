b247ac5e6e265c3274de8d1218ad5ccef0b896d8
Mayra Silva <mayraeduarda2002@gmail.com>
2025-10-23 12:11:45 -0300

    interfaces eq solver step 0

---
+++ interfaces_by_building_an_equation_solver.py
@@ -0,0 +1,5 @@
+# Learn Interfaces by Building an Equation Solver by Mayra Silva
+'''
+I keep this files as I'm studying and taking notes throughout the code, feel free to see
+how I decieded to solve some problems!
+'''
